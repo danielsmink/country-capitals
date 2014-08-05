8b19b07e5745a7fe38f5f99ce2e7e24d029bb454
danielsmink <smink83@gmail.com>
2014-08-05 22:13:19 +0200

    Added watch task and help messages to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,34 +9,41 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     bytediff = require('gulp-bytediff'),
     stylish = require('jshint-stylish'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    paths = {
+    scripts: './app/**/*.js',
+    jsCompiled: 'public/js',
+    scss: './app/scss/**/*.scss',
+    scssDir: './app/scss',
+    css: './public/css'
+  };
 
 require('gulp-help')(gulp);
 
 // Compile compass
-gulp.task('compass', function () {
-  return gulp.src('./app/scss/**/*.scss')
+gulp.task('compass', 'Compiles compass', function () {
+  return gulp.src(paths.scss)
     .pipe(compass({
       config_file: './config.rb',
-      css: 'public/css',
-      sass: 'app/scss'
+      css: paths.css,
+      sass: paths.scssDir
     }))
     // Minify CSS
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest(paths.css));
 });
 
 // Run code through jshint
-gulp.task('jshint', function () {
-  return gulp.src('./app/**/*.js')
+gulp.task('jshint', false, function () {
+  return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
 });
 
 // Run jshint first
-gulp.task('js', ['jshint'], function () {
-  return gulp.src('./app/**/*.js')
+gulp.task('js', 'Minifies JavaScript files', ['jshint'], function () {
+  return gulp.src(paths.scripts)
     .pipe(sourcemaps.init())
     .pipe(concat('country-capitals.min.js', {newLine: ';'}))
     // Annotate before uglify so the code get's min'd properly.
@@ -49,5 +56,14 @@ gulp.task('js', ['jshint'], function () {
     .pipe(uglify({mangle: true}))
     .pipe(bytediff.stop())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./public/js'));
+    .pipe(gulp.dest(paths.jsCompiled));
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', 'Watches JavaScript and sass files', function() {
+  gulp.watch(paths.scripts, ['js']);
+  gulp.watch(paths.scss, ['compass']);
+});
+
+// Default task
+gulp.task('default', 'The default task :-)', ['watch', 'compass', 'js']);
