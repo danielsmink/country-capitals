b8af59c6b8e704fd3ad85c5f8c60dbb4646761ed
danielsmink <smink83@gmail.com>
2014-08-06 20:02:34 +0200

    Update 2014-08-06T18:02:34.147Z

--- app/country-capitals.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/*
- * Contains constants used throughout the application
- */
-angular
-  .module('countryCapitals.config', [])
-  .constant('geoApiUsername', 'danielsmink')
-  .constant('apiBaseUrl', 'http://api.geonames.org/');
--- app/country-capitals.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular
-  .module('countryCapitals', ['ngRoute', 'countryCapitals.config']);
